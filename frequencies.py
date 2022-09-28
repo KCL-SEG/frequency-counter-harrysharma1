b636facd7d7554a4b72896862f9720b3c3e8c658
Harry Sharma <harshit.sharma@kcl.ac.uk>
2022-09-28 21:57:15 +0100

    Finish
    Added for loop to turn all existing items into strings

--- frequencies.py
+++ frequencies.py
@@ -1,9 +1,16 @@
 """Frequencies function."""
 """ENTER YOUR SOLUTION HERE!"""
-
+"""Websites used for syntax:
+    https://www.w3schools.com/python/python_strings_methods.asp
+    https://www.w3schools.com/python/python_lists_methods.asp
+    https://www.w3schools.com/python/python_dictionaries_methods.asp   
+"""
 
 
 def frequencies(items):
+    for i in range(len(items)):
+        items[i]=f'{items[i]}'
+        
     frequencies = {}
     size=len(items)
     items.sort()
@@ -21,4 +28,4 @@ def frequencies(items):
     return frequencies
 
 print(frequencies(['a','a','b','b','b','c']))
-print(frequencies([100, 'Hello', 100, 100, 100]))
+print(frequencies([100, 'Hello', '100', '100', 100]))
