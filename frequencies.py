cc6106407133dc2bd6a94020fde117a45085ef58
Harry Sharma <harshit.sharma@kcl.ac.uk>
2022-09-28 21:49:35 +0100

    First aspect complete
    Frequency table works when the list is of the same type

--- frequencies.py
+++ frequencies.py
@@ -1,7 +1,24 @@
 """Frequencies function."""
 """ENTER YOUR SOLUTION HERE!"""
 
+
+
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+    size=len(items)
+    items.sort()
+    while size>0:
+        amount=0
+        temp=items[0]
+        amount=items.count(items[0])
+        if frequencies.get(temp) is None:
+            frequencies[f"{temp}"]=amount
+        size-=1
+        items.remove(temp)
+        amount=0
+        
+        
     return frequencies
+
+print(frequencies(['a','a','b','b','b','c']))
+print(frequencies([100, 'Hello', 100, 100, 100]))
